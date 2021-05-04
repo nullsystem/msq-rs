71f57c9c72d24a765b0b4dbe9fdef876442b9b88
nullsystem <15316579+nullsystem@users.noreply.github.com>
2021-05-04 18:45:13 +0100

    Multiple of packets send+recv and server amount limit
    NEW: Multiple of packets sent and recieved
    	Now more than 512 bytes worth of info can be used
    NEW: Server amount limit
    	max_servers_on_query(usize)

--- tests/lib_test.rs
+++ tests/lib_test.rs
@@ -1,13 +1,27 @@
 #[tokio::main]
 #[test]
-async fn test_lib() -> std::io::Result<()> {
+async fn test_lib_nt() -> std::io::Result<()> {
     let mut client = msq::MSQClient::new().await?;
     client.connect("hl2master.steampowered.com:27011").await?;
     let servers = client
-        .query(msq::Region::All,
-            msq::Filter::new()
-                .appid(244630)
-        )
+        .query(msq::Region::All, msq::Filter::new().appid(244630))
+        .await?;
+
+    println!("Servers: {}", servers.len());
+    for server in servers {
+        println!("{}", server);
+    }
+    Ok(())
+}
+
+#[tokio::main]
+#[test]
+async fn test_lib_css() -> std::io::Result<()> {
+    let mut client = msq::MSQClient::new().await?;
+    client.connect("hl2master.steampowered.com:27011").await?;
+    client.max_servers_on_query(64);
+    let servers = client
+        .query(msq::Region::All, msq::Filter::new().appid(240))
         .await?;
 
     println!("Servers: {}", servers.len());
