20e91b4aa149355ea023023e0418c1132a85a7fb
mtcw <15316579+mtcw99@users.noreply.github.com>
2021-05-05 01:19:54 +0100

    nand/nor fixed/working, and started a bit on doc
    FIX: nand/nor fixed, now include parameter for conditions amount
    NEW: Bit of docs work, sort of unfinished at the moment

--- src/filter.rs
+++ src/filter.rs
@@ -1,49 +1,76 @@
+//! Filter builder
+//!
+//! # Quick Start
+//!
+//! ```
+//! use msq::Filter;
+//! let filter = Filter::new()
+//!     .appid(240)
+//!     .full(false)
+//!     .map("de_dust2");
+//! ```
+//!
+
+/// Filter builder
+///
+/// Reference: https://developer.valvesoftware.com/wiki/Master_Server_Query_Protocol#Filter
 pub struct Filter {
     filter_str: String,
 }
 
-// Filters list: https://developer.valvesoftware.com/wiki/Master_Server_Query_Protocol#Filter
-
 impl Filter {
+    /// Returns a string representing the filters
     pub fn as_str(&self) -> &str {
         return &self.filter_str;
     }
 
+    /// Returns a new Filter struct, used for string builder
+    ///
+    /// # Examples
+    /// ```
+    /// // Filter
+    /// use msq::Filter;
+    /// let filter = Filter::new()
+    ///     .appid(240)
+    ///     .full(false)
+    ///     .map("de_dust2");
+    /// ```
     pub fn new() -> Filter {
         Filter {
             filter_str: String::from(""),
         }
     }
 
-    // Generic filter: Special
-    fn special(mut self, name: &str) -> Filter {
+    // Append the name
+    fn apn_name(&mut self, name: &str) {
         self.filter_str.push_str(&format!("\\{}\\", name));
-        self
     }
 
     // Generic filter: Boolean
     fn boolean(mut self, name: &str, switch: bool) -> Filter {
-        self.filter_str
-            .push_str(&format!("\\{}\\{}", name, switch as i32));
+        self.apn_name(name);
+        self.filter_str.push_str(&format!("{}", switch as i32));
         self
     }
 
     // Generic filter: String
     fn string(mut self, name: &str, param: &str) -> Filter {
-        self.filter_str.push_str(&format!("\\{}\\{}", name, param));
+        self.apn_name(name);
+        self.filter_str.push_str(param);
         self
     }
 
     // Generic filter: Unsigned integer of 32 bits
     fn uint32(mut self, name: &str, num: u32) -> Filter {
-        self.filter_str.push_str(&format!("\\{}\\{}", name, num));
+        self.apn_name(name);
+        self.filter_str.push_str(&format!("{}", num));
         self
     }
 
     // Generic filter: Vector of strings
     fn vecstr(mut self, name: &str, tags: &Vec<&str>) -> Filter {
         if tags.len() > 0 {
-            self.filter_str.push_str(&format!("\\{}\\", name));
+            self.apn_name(name);
             for tag in tags {
                 self.filter_str.push_str(&format!("{},", tag));
             }
@@ -52,17 +79,20 @@ impl Filter {
         self
     }
 
-    // A special filter, specifies that servers matching any of the following [x] conditions should not be returned
-    pub fn nor(self) -> Filter {
-        self.special("nor")
+    /// A special filter, specifies that servers matching any of the following [x] conditions should not be returned
+    pub fn nor(self, conditions: u32) -> Filter {
+        self.uint32("nor", conditions)
     }
 
-    // A special filter, specifies that servers matching all of the following [x] conditions should not be returned
-    pub fn nand(self) -> Filter {
-        self.special("nand")
+    /// A special filter, specifies that servers matching all of the following [x] conditions should not be returned
+    pub fn nand(self, conditions: u32) -> Filter {
+        self.uint32("nand", conditions)
     }
 
-    // Servers running dedicated
+    /// Filters if the servers running dedicated
+    ///
+    /// # Arguments
+    /// * `is_dedicated` - A bool 
     pub fn dedicated(self, is_dedicated: bool) -> Filter {
         self.boolean("dedicated", is_dedicated)
     }
@@ -127,37 +157,52 @@ impl Filter {
         self.boolean("white", white)
     }
 
-    // Servers with all of the given tag(s) in sv_tags
+    /// Servers with all of the given tag(s) in sv_tags
+    ///
+    /// # Arguments
+    /// * `tags` - A vector of strings which represents a tag from sv_tags
+    ///
+    /// # Example
+    /// ```
+    /// use msq::Filter;
+    /// let filter = Filter::new()
+    ///     .appid(240)
+    ///     .gametype(&vec!["friendlyfire", "alltalk"]);
+    /// ```
     pub fn gametype(self, tags: &Vec<&str>) -> Filter {
         self.vecstr("gametype", tags)
     }
 
-    // Servers with all of the given tag(s) in their 'hidden' tags (L4D2)
+    /// Servers with all of the given tag(s) in their 'hidden' tags (L4D2)
+    /// # Arguments
+    /// * `tags` - A vector of strings which represents a tag from sv_tags
     pub fn gamedata(self, tags: &Vec<&str>) -> Filter {
         self.vecstr("gamedata", tags)
     }
 
-    // Servers with any of the given tag(s) in their 'hidden' tags (L4D2)
+    /// Servers with any of the given tag(s) in their 'hidden' tags (L4D2)
+    /// # Arguments
+    /// * `tags` - A vector of strings which represents a tag from sv_tags
     pub fn gamedataor(self, tags: &Vec<&str>) -> Filter {
         self.vecstr("gamedataor", tags)
     }
 
-    // Servers with their hostname matching [hostname] (can use * as a wildcard)
+    /// Servers with their hostname matching [hostname] (can use * as a wildcard)
     pub fn name_match(self, hostname: &str) -> Filter {
         self.string("name_match", hostname)
     }
 
-    // Servers running version [version] (can use * as a wildcard)
+    /// Servers running version [version] (can use * as a wildcard)
     pub fn version_match(self, ver: &str) -> Filter {
         self.string("version_match", ver)
     }
 
-    // Return only one server for each unique IP address matched
+    /// Return only one server for each unique IP address matched
     pub fn collapse_addr_hash(self, one_server: bool) -> Filter {
         self.boolean("collapse_addr_hash", one_server)
     }
 
-    // Return only servers on the specified IP address (port supported and optional)
+    /// Return only servers on the specified IP address (port supported and optional)
     pub fn gameaddr(self, ipaddr: &str) -> Filter {
         self.string("gameaddr", ipaddr)
     }
